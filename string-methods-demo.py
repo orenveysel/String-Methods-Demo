440cb71d7ba8364a60cb699f6354e54ee1298d92
Veysel ÖREN <orenveysel@outlook.com>
2021-12-29 21:53:49 +0300

    Create string-methods-demo.py

---
+++ string-methods-demo.py
@@ -0,0 +1,37 @@
+website = "http://www.sadikturan.com"
+course  = "Pyhton Kursu: Baştan Sona Python Programlama Rehberiniz (40 saat)"
+
+#-1 message = ' Hello World '.strip()
+#   print(message)
+
+#-2 message = 'www.sadikturan.com'.strip('w.moc')
+#   print(message)
+
+#-3 course = course.lower()
+#   print(course)
+
+#-4 website = website.count('a')
+#   print(website)
+
+#-5 isFound = website.startswith('www') + website.endswith('com')
+#   print(isFound)
+#   result = 1 because only one of them (endswith) is true
+
+#-6 isFound = website.find('.com')
+#   print(isFound)
+
+#-7 message = course.isalpha()   
+#   print(message)      
+
+#-8 message = ' Contents '.center(50,'*')
+#   print(message)
+
+#-9 course = course.replace(' ','-')
+#   print(course)
+
+#-10 message = 'Hello World'.replace('World','There')
+#    print(message)
+
+#-11 course = course.split(' ')
+#    print(course)
+
